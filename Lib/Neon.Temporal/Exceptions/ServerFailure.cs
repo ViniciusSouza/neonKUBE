e4989a3a0414c9bc8f52ed72e5959d771585a2cb
Jack Burns <johncburns1@gmail.com>
2021-03-08 21:05:59 -0800

    Fix documentation.

--- Lib/Neon.Temporal/Exceptions/ServerFailure.cs
+++ Lib/Neon.Temporal/Exceptions/ServerFailure.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------------
-// FILE:	    ChildWorkflowFailure.cs
+// FILE:	    ServerFailure.cs
 // CONTRIBUTOR: Jack Burns
 // COPYRIGHT:	Copyright (c) 2005-2021 by neonFORGE LLC.  All rights reserved.
 //
